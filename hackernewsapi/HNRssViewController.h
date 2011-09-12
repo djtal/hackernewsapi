36583c1466302773607a3f3ba81589eb32c7200e
djtal <guillaume.garcera@gmail.com>
2011-09-12 20:51:28 +0200

    start working on parsong HN rss feed

---
+++ hackernewsapi/HNRssViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNRssViewController.h
+//  hackernewsapi
+//
+//  Created by Guillaume Garcera on 07/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNRssViewController : UITableViewController
+
+@end
---
+++ hackernewsapi/HNRssViewController_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  HNRssViewController_iPhone.h
+//  hackernewsapi
+//
+//  Created by Guillaume Garcera on 07/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HNRssViewController.h"
+
+@interface HNRssViewController_iPhone : HNRssViewController
+
+@end
